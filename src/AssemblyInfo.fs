d9ebda776088ff916361d5fa50d42298a6f882e1
nakamura-to <toshihiro.nakamura@gmail.com>
2013-03-05 22:47:21 +0900

    Change AssemblyInfo

--- src/AssemblyInfo.fs
+++ src/AssemblyInfo.fs
@@ -10,16 +10,20 @@
 // You must not remove this notice, or any other, from this software.
 //----------------------------------------------------------------------------
 
-module AssemblyInfo
+namespace Tranq
+
 open System.Reflection
 open System.Runtime.CompilerServices;
+
 [<assembly:AssemblyDescription("Tranq.dll")>]
-[<assembly:AssemblyCompany("http://soma.codeplex.com/")>]
+[<assembly:AssemblyCompany("https://github.com/nakamura-to/Tranq")>]
 [<assembly:AssemblyTitle("Tranq.dll")>]
 [<assembly:AssemblyCopyright("Copyright the Tranq Team.  All rights reserved.")>]
 [<assembly:AssemblyProduct("Tranq")>]
 [<assembly:AssemblyVersion("0.1.0.0")>]
+
 #if DEBUG
 [<assembly:InternalsVisibleTo("Tranq.Test")>]
 #endif
+
 do()
